a03f377724a9f6eaaebfa1b3248a74fe70c625c0
yacine.in4k <144620343+ycn3310@users.noreply.github.com>
2026-01-23 00:03:26 +0100

    Enable reading from multiple files in cat.c
    Modified the main function to handle multiple files as input.

--- YacineLinuxMimic/source codes/cat.c
+++ YacineLinuxMimic/source codes/cat.c
@@ -4,15 +4,17 @@ int main(int argc, char **argv){
     char str[4096];
     size_t n;
     FILE* file;
-    file = fopen(argv[1], "r");
+    for (int i = 1; i < argc; i++) {
+        file = fopen(argv[i], "r");
 
-    if(file == NULL){
-        printf("couldn't open the file\n");
-        return 0;
-    } 
-    while((n = fread(str, 1, sizeof(str), file))>0){
-        fwrite(str,1,n, stdout);
+        if (file == NULL) {
+            printf("couldn't open the file\n");
+            return 0;
+        }
+        while ((n = fread(str, 1, sizeof(str), file)) > 0) {
+            fwrite(str, 1, n, stdout);
+        }
+        fclose(file);
     }
-    fclose(file);
     return 0;
 }
